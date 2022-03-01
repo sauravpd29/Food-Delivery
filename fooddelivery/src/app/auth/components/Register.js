553b924a574ce48f306cb5631b67e92dec185468
Saurav Prasad <79800720+sauravpd29@users.noreply.github.com>
2022-03-01 22:20:52 +0530

    Food Delivery Frontend

--- fooddelivery/src/app/auth/components/Register.js
+++ fooddelivery/src/app/auth/components/Register.js
@@ -70,7 +70,7 @@ const Register = ({ isAuthenticated, register }) => {
       <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-lg-12 col-xl-11">
           <div class="card text-black">
-            <div class="card-body p-md-5">
+            <div class="card-body p-md-0">
               <div class="row justify-content-center">
                 <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
                   <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
@@ -189,7 +189,7 @@ const Register = ({ isAuthenticated, register }) => {
                     <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
                       <input
                         type="submit"
-                        className="btn btn-info btn-block mt-4"
+                        className="btn btn-info btn-block mt-1"
                       />
                     </div>
                   </form>
